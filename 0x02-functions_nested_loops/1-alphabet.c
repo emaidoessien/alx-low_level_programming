9c47b5273aa6783db4eec931e915dba6fa0da238
emaidoessien <emaidoessien@gmail.com>
2022-07-05 01:47:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - function that print lowercase alphabet followed by new line
+*Return: no return
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}

f15c19d334c8e7773d1a1f5de42714109316fdf1
emaidoessien <emaidoessien@gmail.com>
2022-07-14 07:41:47 -0700

    final commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_number - print int
- * @n: integer
- * Return: void
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
+
 void print_number(int n)
 {
 	unsigned int m, d, count;
@@ -18,6 +19,7 @@ void print_number(int n)
 	{
 		m = n;
 	}
+
 	d = m;
 	count = 1;
 
@@ -26,8 +28,9 @@ void print_number(int n)
 		d /= 10;
 		count *= 10;
 	}
+
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10) + 45);
+		_putchar(((m / count) % 10) + 48);
 	}
 }

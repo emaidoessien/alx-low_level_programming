114a2c9ddf9b26683384247769267ba8ee4fbb08
emaidoessien <emaidoessien@gmail.com>
2022-08-11 12:39:37 -0700

    fresh commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t elemt;
+	int count = 0;
 
-	elemt = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		count++;
 		h = h->next;
-		elemt++;
 	}
-	return (elemt);
+	return (count);
 }
